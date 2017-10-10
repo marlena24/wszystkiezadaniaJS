1bfd767e2739765e8d175c2a73a2ea4cae073067
marlena24 <marika25809@op.pl>
2017-10-10 06:45:35 +0200

    zadanie do zatwierdzenia

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
-var triangleArea
-var a = 2;
-var h = 4;
+function getTriangleArea(a,h) {
+if ( a > 0 || h > 0 )
+	alert('Pawidłowe dane');
+else
+	alert('Nieprawidłowe dane'); 
+return ( a * h / 2 );
+}
+console.log( getTriangleArea(10,6) )
 
-var triangleArea = a*h/2
-
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(3, 7);
+var triangle3Area = getTriangleArea(5, 0);
