4eb5033cba10476357d3817c243b53bfa4630356
marlena24 <marika25809@op.pl>
2017-10-12 14:33:10 +0200

    poprawione 8-6-nowybranch

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var a = 2;
+var b = 4;
+var value = ((a * a) - (2 * a * b) + (b * b));
+
+if (value < 1) {
+	alert("ujemna");
+} if else (value > 1) {
+	alert("dodatnia");
+} else {
+	alert("0");
+}
