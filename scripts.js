a3700ebd2f08a4dfadee13a571343ea85b89b8b9
marlena24 <marika25809@op.pl>
2017-10-14 08:23:59 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 var a = 2;
-var b = 4;
+var b = -4;
 var value = ((a * a) - (2 * a * b) + (b * b));
 
 if (value < 1) {
 	alert("ujemna");
-} if else (value > 1) {
+} else if (value > 1) {
 	alert("dodatnia");
 } else {
 	alert("0");
