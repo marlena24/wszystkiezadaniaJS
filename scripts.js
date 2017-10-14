54aaee584771c7b02e67652ab2800f9a14a19469
marlena24 <marika25809@op.pl>
2017-10-14 09:46:01 +0200

    Update scripts.js
    poprawiona wartosc 1 na 0

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ var a = 2;
 var b = -4;
 var value = ((a * a) - (2 * a * b) + (b * b));
 
-if (value < 1) {
+if (value < 0) {
 	alert("ujemna");
-} else if (value > 1) {
+} else if (value > 0) {
 	alert("dodatnia");
 } else {
 	alert("0");
